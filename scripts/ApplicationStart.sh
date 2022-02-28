6733cefda5be12154d70e3eab07510226ca9d071
root <root@ip-172-31-26-27.us-east-2.compute.internal>
2022-02-28 01:44:22 +0000

    new

---
+++ scripts/ApplicationStart.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ubuntu/qc
+bash build.sh
---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo su
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install docker.io -y
+sudo apt install docker-compose -y
+sudo apt update -y
+sudo apt install ruby-full
+sudo apt install wget
+wget https://aws-codedeploy-us-east-2.s3.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto > /tmp/logfile
+sudo service codedeploy-agent start
+sudo service codedeploy-agent status
+git clone https://github.com/durgap09/qc.git
+cd qc/
